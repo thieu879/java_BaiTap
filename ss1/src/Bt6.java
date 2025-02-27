97b2d8e48c5d29c14da40774ccb453209a7d2659
thieu879 <nguyenthieu11021995@gmail.com>
2025-02-27 16:46:34 +0700

    add Bt6

---
+++ ss1/src/Bt6.java
@@ -0,0 +1,19 @@
+public class Bt6 {
+    public static void main(String[] args) {
+        System.out.println("+----------------------------------+");
+        System.out.println("|          MENU CHỨC NĂNG          |");
+        System.out.println("+----------------------------------+");
+        System.out.println("| 1.  Đăng nhập                    |");
+        System.out.println("| 2.  Đăng ký                      |");
+        System.out.println("| 3.  Xem danh sách sản phẩm       |");
+        System.out.println("| 4.  Tìm kiếm sản phẩm            |");
+        System.out.println("| 5.  Thêm sản phẩm vào giỏ hàng   |");
+        System.out.println("| 6.  Xóa sản phẩm khỏi giỏ hàng   |");
+        System.out.println("| 7.  Xem giỏ hàng                 |");
+        System.out.println("| 8.  Thanh toán                   |");
+        System.out.println("| 9.  Xem lịch sử mua hàng         |");
+        System.out.println("| 10. Thoát                        |");
+        System.out.println("+----------------------------------+");
+        System.out.print("| Vui lòng chọn một chức năng 1-10: ");
+    }
+}
