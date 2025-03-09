4bc87be22f56c600c4bb48a720659c0a7dcbc6f9
thieu879 <nguyenthieu11021995@gmail.com>
2025-03-10 06:26:38 +0700

    abc

--- ss5/src/Bt3.java
+++ ss5/src/Bt3.java
@@ -5,12 +5,16 @@ public class Bt3 {
         System.out.print("Nhập kích thước mảng: ");
         int n = new Scanner(System.in).nextInt();
         int [] arr = new int[n];
+        int countEven = 0;
         for(int i = 0; i < arr.length; i++) {
             System.out.print("Nhập số đi: ");
             arr[i] = new Scanner(System.in).nextInt();
         }
         for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            if(arr[i]%2==0){
+                countEven++;
+            }
         }
+        System.out.println("Tổng số lượng chẵn "+countEven);
     }
 }
