e38e2ffc62a3fae26b4501ad5d551cb1086e6e37
thieu879 <nguyenthieu11021995@gmail.com>
2025-03-25 08:32:03 +0700

    abc

--- ss18/src/Bt8.java
+++ ss18/src/Bt8.java
@@ -1,31 +1,39 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Bt8 {
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(2, "II");
-        map.put(3, "III");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(6, "VI");
-        map.put(7, "VII");
-        map.put(8, "VIII");
-        map.put(9, "IX");
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
         map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
         System.out.print("Nhập số N: ");
         int n = new Scanner(System.in).nextInt();
         if(n<0){
             System.out.println("Đùa nhau à!");
-        }else if(n>10){
+        }else if(n>3999){
             System.out.println("Chưa có đâu!");
         }
+        StringBuilder roman = new StringBuilder();
         for(Map.Entry<Integer, String> key : map.entrySet()){
-            if(key.getKey() == n){
-                System.out.println("Chuyển thành số la mã là: "+key.getValue());
+            int value = key.getKey();
+            String symbol = key.getValue();
+            while (n>=value){
+                roman.append(symbol);
+                n -= value;
             }
         }
+        System.out.println(roman.toString());
     }
 }
